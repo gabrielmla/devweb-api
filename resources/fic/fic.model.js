a846995866dcc2da88887ff66eb63b04f119dd8d
Gabriel Morais <gabmla19@gmail.com>
2018-09-23 16:13:17 -0300

    fic model

--- resources/fic/fic.model.js
+++ resources/fic/fic.model.js
@@ -2,10 +2,54 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var FicSchema = new Schema({
-	profile_name: {
+	title: {
 		type: String,
-		minlength: 5,
-		maxlength: 18
+		required: true,
+		maxlength: 40
+	},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	status: {
+		type: String,
+		enum: ['in progress', 'abandoned', 'complete']
+	},
+	synopsis: {
+		type: String,
+		maxlength: 280,
+		default: 'No synopsis.'
+	},
+	language: {
+		type: String,
+		required: true
+	},
+	category: {
+		type: String,
+		required: true
+	},
+	genre: {
+		type: [String],
+		required: true
+	},
+	adult_content: {
+		type: Boolean,
+		required: true
+	},
+	chapters: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'Chapter'
+	},
+	words: {
+		type: Number
+	},
+	created_at: {
+		type: Date,
+		default: Date.now
+	},
+	updated_at: {
+		type: Date,
+		default: Date.now
 	}
 });
 
