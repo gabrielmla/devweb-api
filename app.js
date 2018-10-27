b7d819f5057fc5897f2f7b38622a2f5a2536ff17
Gabriel Morais <gabmla19@gmail.com>
2018-10-27 19:23:42 -0300

    port 5000

--- app.js
+++ app.js
@@ -1,88 +1,94 @@
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var morgan         = require('morgan');
-var path           = require('path');
-var cors           = require('cors');
-var mongoose       = require('mongoose');
-var passport       = require('passport');
-var session        = require('express-session');
-var MongoStore     = require('connect-mongo')(session);
+var dotenv = require("dotenv").load();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var morgan = require("morgan");
+var path = require("path");
+var cors = require("cors");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 
 // ENVIROMENT VARIABLES
-var PORT = process.env.PORT || 3000;
-var ENV = process.env.ENVIROMENT || 'development'
+var PORT = process.env.PORT || 5000;
+var ENV = process.env.ENVIROMENT || "development";
 
 // Mongoose Connection
-var db = require('./config/db');
+var db = require("./config/db");
 var db_url;
-if (ENV == 'production') {
+if (ENV == "production") {
   db_url = db.url;
 } else {
   db_url = db.local_url;
 }
 
-mongoose.connect(db_url, { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
+mongoose.connect(
+  db_url,
+  { useNewUrlParser: true }
+);
+mongoose.set("useCreateIndex", true);
 
 // Passport Session
-require('./config/passport')(passport);
-app.use(session({
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 60 * 60 // = 60 minutos de sessão
-  }),
-  secret: process.env.SESSION_SECRET || 'local-secret',
-  resave: false,
-  saveUninitialized: false
-}));
+require("./config/passport")(passport);
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 // = 60 minutos de sessão
+    }),
+    secret: process.env.SESSION_SECRET || "local-secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Morgan
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Static files
-app.use('/static', express.static('public'));
-app.use('/api', express.static(__dirname + '/public/apidoc'));
+app.use("/static", express.static("public"));
+app.use("/api", express.static(__dirname + "/public/apidoc"));
 
 // parse application/json
 app.use(bodyParser.json());
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride("X-HTTP-Method-Override"));
 
 // ======= ROUTES
-var corsOptions = require('./config/cors');
-if (ENV == 'production') {
+var corsOptions = require("./config/cors");
+if (ENV == "production") {
   app.use(cors(corsOptions));
 } else {
   app.use(cors());
 }
 
-app.get('/', (req, res) => {
-	res.send('Fanfic API.');
+app.get("/", (req, res) => {
+  res.send("Fanfic API.");
 });
 
-var authRoutes = require('./resources/auth/auth.router');
-app.use('/auth', authRoutes);
+var authRoutes = require("./resources/auth/auth.router");
+app.use("/auth", authRoutes);
 
-var userRoutes = require('./resources/user/user.router');
-app.use('/user', userRoutes);
+var userRoutes = require("./resources/user/user.router");
+app.use("/user", userRoutes);
 
-var ficRoutes = require('./resources/fic/fic.router');
-app.use('/fic', ficRoutes);
+var ficRoutes = require("./resources/fic/fic.router");
+app.use("/fic", ficRoutes);
 
-var chapterRoutes = require('./resources/chapter/chapter.router');
-app.use('/chapter', chapterRoutes);
+var chapterRoutes = require("./resources/chapter/chapter.router");
+app.use("/chapter", chapterRoutes);
 
 // start app ===============================================
 app.listen(PORT);
-console.log('Deployed on port ' + PORT);
+console.log("Deployed on port " + PORT);
 
 // expose app
 exports = module.exports = app;
--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 // config/db.js
-    module.exports = {
-      url : "",
-      local_url: "mongodb://localhost:27017/fanfic-dev"
-    }
+module.exports = {
+  url: `mongodb://${proccess.env.DB_USER}:${
+    proccess.env.DB_PASSWORD
+  }@ds143683.mlab.com:43683/fanfic-p3`,
+  local_url: "mongodb://localhost:27017/fanfic-dev"
+};
