a1d8c4acc3e619af1ab0d5cede4d54aa852f4c68
Gabriel Morais <gabmla19@gmail.com>
2018-11-24 15:09:48 -0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
 module.exports = {
-  url: "mongodb://" + proccess.env.DB_USER + ":" + proccess.env.DB_PASSWORD + "@ds143683.mlab.com:43683/fanfic-p3",
+  url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds143683.mlab.com:43683/fanfic-p3`,
   local_url: "mongodb://localhost:27017/fanfic-dev"
 };
